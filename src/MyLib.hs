935841049e0c4b2470fdb1388f2a64ec531938d5
Matt Hunzinger <matthunz2@gmail.com>
2024-12-11 22:01:29 -0500

    feat: setup CLI

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -2,9 +2,34 @@ module MyLib (run) where
 
 import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy.Char8 as BS
+import Options.Applicative
 import Parse
 
-run :: IO ()
-run = case parse "" "123" of
+data Sample = Sample
+  { src :: String
+  }
+
+sample :: Parser Sample
+sample =
+  Sample
+    <$> argument
+      str
+      ( metavar "SOURCE"
+          <> help "Source text to parse"
+      )
+
+runParse :: Sample -> IO ()
+runParse opts = case parse "" (src opts) of
   Left err -> print err
   Right expr -> putStrLn (BS.unpack (encodePretty expr))
+
+run :: IO ()
+run = runParse =<< execParser opts
+  where
+    opts =
+      info
+        (sample <**> helper)
+        ( fullDesc
+            <> progDesc "Print a greeting for TARGET"
+            <> header "hello - a test for optparse-applicative"
+        )
